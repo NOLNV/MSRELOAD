dc4ab55fef45ad1590241a4baef1e96ca54750a4
bekey <bekey@sharklasers.com>
2014-02-07 12:51:55 +0100

    Messed up commit. New commit for droppign and picking up gunz

--- Assets/testGunn.js
+++ Assets/testGunn.js
@@ -8,25 +8,36 @@ var nextshot: float = 0.0;
 var gunfire: ParticleSystem;
 var damage: int;
 
+var isEquipped = false;
+
 function Start () {
 
 }
 
 function Update () {
-	
-	transform.position = owner.position;
-	
-	transform.LookAt(crosshair);
-	
-	if (Input.GetButton("Fire1") && Time.time > nextshot){
-		Gunshots.Emit(1);
-		audio.Play();
-		nextshot = Time.time + ROF;
-		gunfire.Emit(1);
-				
-				}
-			}
+	if(isEquipped) {
+		transform.position = owner.position;
+		transform.LookAt(crosshair);
 		
+		if (Input.GetButton("Fire1") && Time.time > nextshot){
+			Gunshots.Emit(1);
+			audio.Play();
+			nextshot = Time.time + ROF;
+			gunfire.Emit(1);
+		}
+	} else {
+	}
+}
 function OnParticleCollision (other : GameObject) {
 		other.collider.SendMessage("ApplyDamage", damage); 
+}
+
+//This is so bad having it in this module I bet. Gotta move this to gunHolder.js
+function OnTriggerEnter(other : Collider) {
+	var gunHolder = owner.GetComponent(gunHolder);
+	if (gunHolder.hasEquipped == false) {
+		gunHolder.hasEquipped = true;
+		gunHolder.Gun = gameObject;
+		isEquipped = true;
 	}
+}
