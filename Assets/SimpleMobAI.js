f2256f1d23ec5c94aac3b48633e54096c35a8023
NOLNV <Antonrisberg@gmail.com>
2014-02-11 03:01:58 +0100

    Some Bullshit, models etcetera
    yellow bastard hoverhead

--- Assets/SimpleMobAI.js
+++ Assets/SimpleMobAI.js
@@ -4,7 +4,7 @@ var Health: int = 20;
 var Deathobject : Transform;
 var speed: float = 0.01;
 var prey : GameObject;
-
+var hunting : boolean = true;
 
 function Start () {
 //Get prey
@@ -14,10 +14,13 @@ prey = GameObject.FindWithTag ("Player");
 
 function Update () {
 //stalk prey
+
+if (hunting == true){
 transform.LookAt(prey.transform.position);
 
 
 transform.position = Vector3.MoveTowards(transform.position, prey.transform.position, speed);
+}
 
 //know how to die
 	if(Health <= 0)
---
+++ Assets/findgroundmobz.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var Shadow : Transform;
+
+function Start () {
+
+	//Instantiate(Shadow, transform.position, Quaternion.identity);
+
+}
+
+
+
+function Update () {
+		var hit : RaycastHit;
+		if (Physics.Raycast (transform.position, -Vector3.up, hit)) {
+						
+			Shadow.position = hit.point;
+		}
+}
